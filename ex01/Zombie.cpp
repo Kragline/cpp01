35cd1f59f559ed551a97984f8d3325659218f5b8
Arman Arakelyan <armarake@c2r2s1.42yerevan.am>
2025-09-10 17:32:46 +0400

    ex01

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,27 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+	_name = "Gagas";
+}
+
+Zombie::Zombie(std::string name)
+{
+	_name = name;
+	std::cout << _name << " was born!" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << _name << " was destroyed!" << std::endl;
+}
+
+void	Zombie::announce()
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	_name = name;
+}
